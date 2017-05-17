064935827d5ff1b53d164c41ae8c3ace9440e9f3
Aleksey Rochev <alexrochevs@gmail.com>
2017-05-17 13:59:56 +0500

    Created xcode project

---
+++ DFS/DFS/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  DFS
+//
+//  Created by Aleksey Rochev on 17/05/2017.
+//  Copyright © 2017 AlekseyRochev. All rights reserved.
+//
+
+import Foundation
+
+print("Lab 2 \"Depth-first search\"  \n")
+print("Getting edges from adjacency matrix: \n")
+
+
+
+
